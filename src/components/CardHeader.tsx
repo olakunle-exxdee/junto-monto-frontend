b20ee7863fb8ac12d06b561a6eb7c05440c74019
Olakunle Ogunsolu <saheedogunsolu2016@gmail.com>
2023-04-27 13:17:23 +0200

    2nd commit

--- src/components/CardHeader.tsx
+++ src/components/CardHeader.tsx
@@ -1,29 +1,4 @@
-import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/20/solid';
 import FilterCombox from './FilterCombox';
-
-const items = [
-  {
-    id: 1,
-    title: 'Back End Developer',
-    department: 'Engineering',
-    type: 'Full-time',
-    location: 'Remote',
-  },
-  {
-    id: 2,
-    title: 'Front End Developer',
-    department: 'Engineering',
-    type: 'Full-time',
-    location: 'Remote',
-  },
-  {
-    id: 3,
-    title: 'User Interface Designer',
-    department: 'Design',
-    type: 'Full-time',
-    location: 'Remote',
-  },
-];
 
 export default function Example() {
   return (
