2fe9eb2e2d03094f306804c3fe798e2f9132d13f
Olakunle Ogunsolu <saheedogunsolu2016@gmail.com>
2023-04-27 17:11:27 +0200

    filtered by state not done yet

--- src/components/FilterCombox.tsx
+++ src/components/FilterCombox.tsx
@@ -1,19 +1,28 @@
+
+
+const gendeCategory = ['male', 'female'];
 export default function Example() {
   return (
-    <div className='flex '>
+    <div className='flex'>
       <label
         htmlFor='location'
         className=' content-center block text-md font-medium leading-6 text-gray-900 m-1 mr-4 '>
-        Location:
+        Sort:
       </label>
       <select
-        id='location'
-        name='location'
+        id='gender'
+        name='gender'
         className=' block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6'
-        defaultValue='Canada'>
-        <option>United States</option>
-        <option>Canada</option>
-        <option>Mexico</option>
+        defaultValue=''
+        onChange={(e) => console.log(e.target.value)}>
+        <option value=''>Gender</option>
+        {gendeCategory.map((c) => {
+          return (
+            <option key={c} value={c}>
+              {c}
+            </option>
+          );
+        })}
       </select>
     </div>
   );
--- src/components/FilterSiderBar.tsx
+++ src/components/FilterSiderBar.tsx
@@ -1,35 +1,64 @@
 import React from 'react';
 const people = [
-  { id: 1, name: 'Annette Black' },
-  { id: 2, name: 'Cody Fisher' },
-  { id: 3, name: 'Courtney Henry' },
-  { id: 4, name: 'Kathryn Murphy' },
-  { id: 5, name: 'Theresa Webb' },
+  'santa catarina',
+  'roraima',
+  'paraná',
+  'tocantins',
+  'goiás',
+  'espírito santo',
+  'minas gerais',
+  'mato grosso',
+  'mato grosso do sul',
+  'acre',
+  'pará',
+  'amapá',
+  'distrito federal',
+  'rio de janeiro',
+  'alagoas',
+  'paraíba',
+  'pernambuco',
+  'bahia',
+  'ceará',
+  'maranhão',
+  'piauí',
+  'rio grande do sul',
+  'amazonas',
+  'rio grande do norte',
+  'são paulo',
+  'sergipe',
+  'rondônia',
 ];
 
 export default function Example() {
+  const [state, setState] = React.useState(false);
   return (
     <div className='col-span-1 mt-12 sm:col-span-full md:col-span-1 lg:col-span-1  '>
       <fieldset>
         <legend className='text-base font-semibold leading-6 text-gray-900'>
-          Members
+          FilterBy State
         </legend>
         <div className='mt-4 divide-y divide-gray-200 border-b border-t border-gray-200'>
-          {people.map((person, personIdx) => (
-            <div key={personIdx} className='relative flex items-start py-4'>
+          {people.map((person) => (
+            <div key={person} className='relative flex items-start py-4'>
               <div className='min-w-0 flex-1 text-sm leading-6'>
                 <label
-                  htmlFor={`person-${person.id}`}
-                  className='select-none font-medium text-gray-900'>
-                  {person.name}
+                  htmlFor={person}
+                  className='select-none font-medium text-gray-900 capitalize'>
+                  {person}
                 </label>
               </div>
               <div className='ml-3 flex h-6 items-center'>
                 <input
-                  id={`person-${person.id}`}
-                  name={`person-${person.id}`}
+                  id={person}
+                  name={person}
                   type='checkbox'
+                  checked={state}
                   className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600'
+                  value={person}
+                  onClick={(e) => {
+                    console.log(e.target);
+                    setState(!state);
+                  }}
                 />
               </div>
             </div>
