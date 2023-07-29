c63a90e802acfd731b9e930b3b83c40873655afc
olakunle-exxdee <saheedogunsolu2016@gmail.com>
2023-07-29 10:28:08 +0200

    testing

--- src/components/PeopleWrapper.test.tsx
+++ src/components/PeopleWrapper.test.tsx
@@ -2,11 +2,31 @@ import { render, screen } from '@testing-library/react';
 import PeopleWrapper from './PeopleWrapper';
 import { MemoryRouter } from 'react-router-dom';
 
-test('', () => {
-  render(
-    <MemoryRouter>
-      <PeopleWrapper />
-    </MemoryRouter>
-  );
-  screen.debug();
+// src/setupTests.js
+import { server } from '../mocks/server';
+// Establish API mocking before all tests.
+
+beforeAll(() => {
+  server.listen({ onUnhandledRequest: 'error' });
+});
+
+afterEach(() => {
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  server.close();
+});
+describe('describe', () => {
+  beforeEach(async () => {
+    await render(
+      <MemoryRouter>
+        <PeopleWrapper />
+      </MemoryRouter>
+    );
+  });
+  test('test running', async () => {
+    const tool = await screen.findAllByText('hello');
+    expect(tool).toBeInTheDocument();
+  });
 });
